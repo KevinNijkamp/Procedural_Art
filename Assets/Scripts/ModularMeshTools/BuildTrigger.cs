5c8dff0d8e980d1638a65b5a8be7224b8bea6629
Kevin Nijkamp <Kevinnijkamp2000@gmail.com>
2023-05-31 16:06:25 +0200

    project setup

---
+++ Assets/Scripts/ModularMeshTools/BuildTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Demo {
+	public class BuildTrigger : MonoBehaviour {
+		public KeyCode BuildKey = KeyCode.Space;
+
+		Shape Root;
+
+		void Start() {
+			Root=GetComponent<Shape>();
+		}
+
+		void Update() {
+			if (Input.GetKeyDown(BuildKey)) {
+				if (Root!=null) {
+					Root.Generate();
+				}
+			}
+		}
+	}
+}
