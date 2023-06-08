245fd67be104c1b8e0a70b22c073b5f07846eb52
Kevin Nijkamp <36000472+KevinNijkamp@users.noreply.github.com>
2023-06-08 02:14:02 +0200

    Environment changes
    tested new shapes and ground

--- Assets/Scripts/CamSwitch.cs
+++ Assets/Scripts/CamSwitch.cs
@@ -6,18 +6,40 @@ public class CamSwitch : MonoBehaviour
 {
     public GameObject cam1;
     public GameObject cam2;
+    public GameObject cam3;
+    public GameObject cam4;
 
     void Update () {
-        if (Input.GetButtonDown("1Key"))
+        if (Input.GetButtonDown("1Knop"))
         {
             cam1.SetActive(true);
             cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
         }
 
-        if (Input.GetButtonDown("2Key"))
+        if (Input.GetButtonDown("2Knop"))
         {
             cam1.SetActive(false);
             cam2.SetActive(true);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+        }
+
+        if (Input.GetButtonDown("3Knop"))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(true);
+            cam4.SetActive(false);
+        }
+
+        if (Input.GetButtonDown("4Knop"))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(true);
         }
     }
 }
